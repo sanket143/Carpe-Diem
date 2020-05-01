ee3876e1baa97607f80481c404c9707b008b10f2
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-05-01 12:56:46 +0530

    Added types

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,10 +2,36 @@
   <div>
     <h1>Find something to do.</h1>
 
-    <div class="buttons">
-      <a href="https://twitter.com/Dave_Conner" class="btn btn-4"><span>Hover</span></a>
+    <div class="activity-buttons">
+      <span class="activity-btn">[ Education ]</span>
+      <span class="activity-btn">[ Recreational ]</span>
+      <span class="activity-btn">[ Social ]</span>
+      <span class="activity-btn">[ DIY ]</span>
+      <span class="activity-btn">[ Charity ]</span>
+      <span class="activity-btn">[ Cooking ]</span>
+      <span class="activity-btn">[ Relaxation ]</span>
+      <span class="activity-btn">[ Music ]</span>
+      <span class="activity-btn">[ Busywork ]</span>
     </div>
   </div>
 
 </template>
 
+<style>
+.activity-buttons {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.activity-btn {
+  cursor: pointer;
+  margin: 10px;
+}
+
+.activity-btn:hover {
+  color: crimson;
+  transition: 0.2s;
+}
+
+</style>
